249f42c9868c98991dcbb46d8caf1b2cef3cf748
João Oliveira <joaooliveira1999@outlook.com>
2023-02-08 22:56:56 -0300

    slider fail

--- js/script.js
+++ js/script.js
@@ -8,29 +8,29 @@ menu.addEventListener("click", () => {
   NavMenu.classList.toggle("ativo");
 });
 
-//Glider
-const corousel = document.querySelector(".js-carousel");
+//slider
 
-new Glider(corousel, {
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  draggable: true,
-  dots: ".js-carousel--simple-dots",
-  scrollLock: true,
-  responsive: [
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 900,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    }
-  ],
-});
+let time = 1000,
+  currentImageIndex = 0,
+  images = document.querySelectorAll(".slider img");
+max = images.length;
+
+function nextImage() {
+  images[currentImageIndex].classList.remove("selected");
+
+  currentImageIndex++;
+
+  if (currentImageIndex >= max) {
+    currentImageIndex = 0;
+  }
+
+  images[currentImageIndex].classList.add("selected");
+}
+
+function start() {
+  setInterval(() => {
+    //change image
+    nextImage;
+  }, time);
+}
+window.addEventListener("load", start);

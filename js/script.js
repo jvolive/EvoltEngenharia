c2d91add06f748f28cd2d7caf6975f21cd6c6503
João Oliveira <joaooliveira1999@outlook.com>
2023-02-13 22:38:36 -0300

    13/02

--- js/script.js
+++ js/script.js
@@ -9,28 +9,3 @@ menu.addEventListener("click", () => {
 });
 
 //slider
-
-let time = 1000,
-  currentImageIndex = 0,
-  images = document.querySelectorAll(".slider img");
-max = images.length;
-
-function nextImage() {
-  images[currentImageIndex].classList.remove("selected");
-
-  currentImageIndex++;
-
-  if (currentImageIndex >= max) {
-    currentImageIndex = 0;
-  }
-
-  images[currentImageIndex].classList.add("selected");
-}
-
-function start() {
-  setInterval(() => {
-    //change image
-    nextImage;
-  }, time);
-}
-window.addEventListener("load", start);

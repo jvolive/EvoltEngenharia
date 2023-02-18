c4e67cd60049075159afbd684a97a9a714777831
João Oliveira <joaooliveira1999@outlook.com>
2023-02-18 15:58:01 -0300

    slideshow working

--- js/script.js
+++ js/script.js
@@ -9,3 +9,26 @@ menu.addEventListener("click", () => {
 });
 
 //slider
+new Glider(document.querySelector(".glider"), {
+  // Mobile-first defaults
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  scrollLock: true,
+  duration: 0.5,
+  arrows: {
+    prev: ".glider-prev",
+    next: ".glider-next",
+  },
+  responsive: [
+    {
+      // screens greater than >= 1024px
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        itemWidth: 150,
+        duration: 0.25,
+      },
+    },
+  ],
+});

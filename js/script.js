02fbd552b6956d3cc7d9238b05377bb8c7eca0a5
João Oliveira <joaooliveira1999@outlook.com>
2023-03-11 18:11:18 -0300

    update pages

--- js/script.js
+++ js/script.js
@@ -8,6 +8,21 @@ menu.addEventListener("click", () => {
   NavMenu.classList.toggle("ativo");
 });
 
+//whats fomulario
+const form = document.querySelector("#meu-formulario");
+form.addEventListener("submit", () => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const message = `Nova mensagem de contato:\n\nNome: ${formData.get(
+    "nome"
+  )}\nE-mail: ${formData.get("email")}\nMensagem: ${formData.get("mensagem")}`;
+  const phoneNumber = "5542999131418"; // substitua pelo número de telefone para o qual deseja enviar a mensagem
+  const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+    message
+  )}`;
+  window.open(url);
+});
+
 //slider
 const swiper = new Swiper(".swiper", {
   // Optional parameters

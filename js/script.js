a7fc9a0a8f6fdd521dad5f2819b51cc1e5c6035a
João Oliveira <joaooliveira1999@outlook.com>
2023-03-11 03:01:20 -0300

    update pages

--- js/script.js
+++ js/script.js
@@ -29,4 +29,10 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+  },
 });

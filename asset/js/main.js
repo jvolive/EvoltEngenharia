0ed11e4474e13321a74a5acef7e0b833df07768d
João Oliveira <joaooliveira1999@outlook.com>
2023-03-14 20:48:52 -0300

    update pages

--- asset/js/main.js
+++ asset/js/main.js
@@ -8,17 +8,57 @@ menu.addEventListener("click", () => {
   NavMenu.classList.toggle("ativo");
 });
 
-//whats fomulario
-const form = document.querySelector("#meu-formulario");
-form.addEventListener("submit", () => {
+//formulario
+const form = document.getElementById("form");
+const campos = document.querySelectorAll(".required");
+const spans = document.querySelectorAll(".span-required");
+const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const formData = new FormData(event.target);
-  const message = `Nova mensagem de contato:\n\nNome: ${formData.get(
-    "nome"
-  )}\nE-mail: ${formData.get("email")}\nMensagem: ${formData.get("mensagem")}`;
-  const phoneNumber = "5542999131418"; // substitua pelo número de telefone para o qual deseja enviar a mensagem
-  const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
-    message
-  )}`;
-  window.open(url);
+  nameValidate();
+  emailValidate();
 });
+
+function setError(index) {
+  campos[index].style.border = "2px solid #FF0000";
+  spans[index].style.display = "block";
+}
+
+function removeError(index) {
+  campos[index].style.border = "";
+  spans[index].style.display = "none";
+}
+
+function nameValidate() {
+  if (campos[0].value.length < 3) {
+    setError(0);
+  } else {
+    removeError(0);
+  }
+}
+
+function emailValidate() {
+  if (!emailRegex.test(campos[1].value)) {
+    setError(1);
+  } else {
+    removeError(1);
+  }
+}
+
+function mainPasswordValidate() {
+  if (campos[2].value.length < 8) {
+    setError(2);
+  } else {
+    removeError(2);
+    comparePassword();
+  }
+}
+
+function comparePassword() {
+  if (campos[2].value == campos[3].value && campos[3].value.length >= 8) {
+    removeError(3);
+  } else {
+    setError(3);
+  }
+}
